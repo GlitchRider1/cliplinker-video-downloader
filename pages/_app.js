4070048857a2cc9ace2613f50dacbd77930ba5ac
GlitchRider1 <glitchrider831@gmail.com>
2025-07-15 23:02:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '../styles/globals.css';
-import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src={`https://www.paypal.com/sdk/js?client-id=AUV9kiTLZ5IdcKHTKhd9s_JPha7d9ZUj9miMdlpHh3DOKml1rsXwVll02T4brHQaszczEPZ-lqCYClC4&currency=USD`}
-        strategy="beforeInteractive"
-      />
-      <Component {...pageProps} />
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
