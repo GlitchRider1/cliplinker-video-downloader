e5fbb6f2f8dc08a5e5294287157fe134065cef8c
GlitchRider1 <glitchrider831@gmail.com>
2025-07-15 20:16:14 +0100

    Delete Pages directory

--- Pages/contact.js
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-  return (
-    <div style={{maxWidth: 600, margin: 'auto', padding: 20}}>
-      <h1>Contact Us</h1>
-      <p>For support, please email <a href="mailto:support@cliplinker.com">support@cliplinker.com</a>.</p>
-    </div>
-  );
-}
